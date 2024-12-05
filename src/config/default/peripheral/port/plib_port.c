f38bff50ac6814f6693ea37a6b965a24dffbc4da
Andy Appeldorn <andy.appeldorn@microchip.com>
2024-12-05 12:33:23 -0600

    stable - enabled pull up on int line, dv is responsive

--- src/config/default/peripheral/port/plib_port.c
+++ src/config/default/peripheral/port/plib_port.c
@@ -84,7 +84,8 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PMUX[13] = 0x0U;
 
    /************************** GROUP 1 Initialization *************************/
-   PORT_REGS->GROUP[1].PORT_PINCFG[7] = 0x2U;
+   PORT_REGS->GROUP[1].PORT_OUT = 0x80U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[7] = 0x6U;
    PORT_REGS->GROUP[1].PORT_PINCFG[24] = 0x1U;
    PORT_REGS->GROUP[1].PORT_PINCFG[25] = 0x1U;
 
